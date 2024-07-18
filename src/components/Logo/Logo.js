09805d1de2c30403cd9956f9e899b50a8761c818
Juris Raudulis <juris.raudulis@outlook.com>
2024-07-18 19:09:42 +0100

    initial commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () =>{
+ return(
+  <div className='ma4 mt0'>
+    <Tilt style={{ height: '100px', width: '100px', backgroundColor: 'grey' }}>
+     <div>
+     </div>
+    </Tilt>
+  </div>
+ );
+}
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () =>{
+ return(
+  <nav>
+   <p className='f3 link dim black underline pa3 pointer'>sign in</p>
+  </nav>
+ );
+}
+
+export default Navigation;

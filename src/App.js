e951bfd05380eebd8fa16375d2eaafd3b000df2b
Juris Raudulis <juris.raudulis@outlook.com>
2024-07-22 19:33:40 +0100

    uploading progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -7,17 +8,30 @@ import ImageInputLink from './components/ImageInputLink/ImageInputLink';
 
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  render() {
   return (
     <div className="App">
-      <ParticlesBg type="cobweb" bg={true} />
+      <ParticlesBg color="#eefad3" type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageInputLink />
-            {/*<FaceRecognition />}*/}
+      <ImageInputLink onInputChange={this.onInputChange} />
+      {/*<FaceRecognition />}*/}
     </div>
   );
+  } 
 }
 
 export default App;
